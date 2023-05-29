274f880eb5101951a3388dc13d2db0b3d0fb97ed
Mansour Alawi <mansour.alawi@gmail.com>
2023-05-29 07:33:45 -0400

    update args module

--- exm/args/args.py
+++ exm/args/args.py
@@ -12,24 +12,25 @@ from exm.utils import chmod
 from exm.io import createFolderStruc
 
 
+
 class Args:
     def __init__(self):
         pass
 
-    def set_params(
-        self,
-        project_path="",
-        codes=list(range(7)),
-        fovs=None,
-        ref_code=0,
-        thresholds=[200, 300, 300, 200],
-        spacing=[1.625, 1.625, 4.0],
-        Create_directroy_Struc=False,
-        permission=False,
-    ):
-
+    def set_params(self,
+                project_path = '',
+                codes = list(range(7)),
+                fovs = None,
+                ref_code = 0,
+                thresholds = [200,300,300,200],
+                spacing = [1.625,1.625,4.0],
+                gene_digit_csv = 'gene_list.csv', #'/mp/nas3/ruihan/20230308_celegans/code0/gene_list.csv'
+                permission = False,
+                Create_directroy_Struc = False,
+                args_file_name = None,
+                ):
+        
         r"""Sets parameters for running alignment code.
-
         :param str project_path: path to project data.
         :param list codes: a list of integers, where each integer represents a code.
         :param list fovs: a list of integers, where each integer represents a field of view.
@@ -38,15 +39,21 @@ class Args:
         :param str align_z_init: path to ``.pkl`` file that has initial :math:`z`-alignment positions.
         :param bool Create_directroy_Struc: If ``True``, Create the working folders stucture for the project path. Default: ``False``
         :param bool permission: Give other users the permission to read and write on the generated files (linux and macOS only). Default: ``False``
+        :param str args_file_name: Pickle file name to store the project Args. Default: ``args.pkl``
         """
         self.project_path = project_path
         self.codes = codes
-        self.fovs = fovs
-        self.ref_code = ref_code
         self.thresholds = thresholds
         self.spacing = spacing
         self.permission = permission
-
+        self.ref_code = ref_code
+        
+        # Housekeeping
+        self.code2num = {'a':'0','c':'1','g':'2','t':'3'}
+        self.colors = ['red','yellow','green','blue']
+        self.colorscales = ['Reds','Oranges','Greens','Blues']
+        self.channel_names = ['640','594','561','488','405']
+        
         # Input ND2 path
         self.nd2_path = os.path.join(
             self.project_path, "code{}/Channel{} SD_Seq000{}.nd2"
@@ -61,7 +68,7 @@ class Args:
             self.fovs = fovs
 
         # Output h5 path
-        self.processed_path = os.path.join(self.project_path, "processed")
+        self.processed_path = os.path.join(self.project_path, "processed_ruihan")
         self.h5_path = os.path.join(self.processed_path, "code{}/{}.h5")
         self.tform_path = os.path.join(self.processed_path, "code{}/tforms/{}.txt")
 
@@ -73,14 +80,20 @@ class Args:
 
         self.work_path = self.project_path + "puncta/"
 
+        self.gene_digit_csv = gene_digit_csv
+
         if Create_directroy_Struc:
             createFolderStruc(project_path, codes)
 
-        with open(os.path.join(self.project_path, "args.pkl"), "wb") as f:
+        if args_file_name == None:
+            args_file_name = 'args.pkl'
+
+        with open(os.path.join(self.project_path, args_file_name), "wb") as f:
             pickle.dump(self.__dict__, f)
 
         if permission:
             chmod(self.project_path)
+
 
     # load parameters from a pre-set .pkl file
     def load_params(self, param_path):
@@ -92,13 +105,15 @@ class Args:
         with open(os.path.abspath(param_path), "rb") as f:
             self.__dict__.update(pickle.load(f))
 
-    def print(self):
-        r"""Prints all attributes."""
-        for attr in dir(self):
-            if not attr.startswith("__"):
-                print(attr, getattr(self, attr))
 
-    def tree(self):
+    def print(self):
+        r"""Prints all attributes.
+        """
+        for key, value in self.__dict__.items():
+            print(f"{key}: {value}")
+
+
+    def list_output_directory(self):
         r"""Lists the files in the output directory."""
         for root, dirs, files in os.walk(self.processed_path):
             level = root.replace(self.processed_path, "").count(os.sep)
@@ -107,6 +122,7 @@ class Args:
             subindent = " " * 4 * (level + 1)
             for f in files:
                 print("{}{}".format(subindent, f))
+                
 
     def progress(self):
         """visualize_progress ``(self)``"""
@@ -132,12 +148,9 @@ class Args:
                 ):
                     result[fov, code_index] = 4
                     continue
-
-                if os.path.exists(
-                    self.work_path + "/fov{}/coords_total_code{}.pkl".format(fov, code)
-                ):
-
-                    result[fov, code_index] = 3
+                        
+                if os.path.exists(self.work_path + '/fov{}/coords_total_code{}.pkl'.format(fov,code)):
+                    result[fov,code_index] = 3
                     continue
 
                 try:
